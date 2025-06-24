3d60f26f0bbbece258c108c1382ebe5a37e8ca6b
Caue Nandi Joaquim <caue.nj@gmail.com>
2025-06-23 22:05:23 -0300

    feat: API get post

--- backend/app/Http/Controllers/Api/PostController.php
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
-{
-    //
+{    
+
+    public function index()
+        {
+            $posts = DB::select("
+                SELECT 
+                    posts.id,
+                    posts.content AS conteudo,
+                    posts.image_path AS urlImg,
+                    users.name AS nome,
+                    users.username,
+                    users.avatar
+                FROM posts
+                JOIN users ON users.id = posts.user_id
+                ORDER BY posts.created_at DESC
+            ");
+
+            return response()->json($posts);
+        }
+
 }
