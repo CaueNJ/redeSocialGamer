08a9052ffe814b48ce769b5a763334e9110f85d8
Caue Nandi Joaquim <caue.nj@gmail.com>
2025-06-25 23:34:41 -0300

    feat: backend login/register

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
@@ -12,3 +13,6 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{username}', [PostController::class, 'showByUsername']);
 
 Route::get('/profile/{username}', [ProfileController::class, 'show']);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
